161ed75c903c245f6ffa86f20000768dbe8d487e
Hakim Benyoucef <benyoucef.hakim@gmail.com>
2020-05-13 23:34:18 +0200

    make NavBar more responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,12 @@
-import React, { Component } from "react";
-import { Container, Responsive, Menu, Image, Button } from "semantic-ui-react";
+import React, { Component, Fragment } from "react";
+import {
+  Container,
+  Responsive,
+  Menu,
+  Image,
+  Button,
+  Grid,
+} from "semantic-ui-react";
 import { NavLink, withRouter } from "react-router-dom";
 import { setAuthUser } from "../store/actions/authUser";
 import { connect } from "react-redux";
@@ -42,6 +49,69 @@ class NavBar extends Component {
               </Menu.Item>
             </Menu.Menu>
           )}
+        </Responsive>
+        <Responsive as={Fragment} minWidth={375} maxWidth={650}>
+          <Grid columns={2} padded="vertically">
+            {this.props.authUser && (
+              <Grid.Row>
+                <Grid.Column>
+                  <Image
+                    src={user && user.avatarURL}
+                    avatar
+                    spaced="right"
+                    verticalAlign="bottom"
+                  />
+                  {user && user.name}
+                </Grid.Column>
+                <Grid.Column verticalAlign="bottom" textAlign="right">
+                  <Button content="Logout" negative onClick={this.logout} />
+                </Grid.Column>
+              </Grid.Row>
+            )}
+            <Grid.Row>
+              <Grid.Column width={16}>
+                <Menu pointing secondary widths={3}>
+                  <Menu.Item name="home" as={NavLink} to="/" exact />
+                  <Menu.Item name="new question" as={NavLink} to="/add" />
+                  <Menu.Item
+                    name="leader board"
+                    as={NavLink}
+                    to="/leaderboard"
+                  />
+                </Menu>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Responsive>
+        <Responsive as={Fragment} maxWidth={374}>
+          <Grid padded="vertically" columns={1}>
+            {this.props.authUser && (
+              <Grid.Row>
+                <Grid.Column>
+                  <Image
+                    src={user && user.avatarURL}
+                    avatar
+                    spaced="right"
+                    verticalAlign="bottom"
+                  />
+                  <Button content="Logout" negative onClick={this.logout} />
+                </Grid.Column>
+              </Grid.Row>
+            )}
+            <Grid.Row>
+              <Grid.Column>
+                <Menu pointing secondary widths={3}>
+                  <Menu.Item name="home" as={NavLink} to="/" exact />
+                  <Menu.Item name="new question" as={NavLink} to="/add" />
+                  <Menu.Item
+                    name="leader board"
+                    as={NavLink}
+                    to="/leaderboard"
+                  />
+                </Menu>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Responsive>
       </Container>
     );
